deb087dd55470aaec7e170f9b53de5de8dedc6ed
Rafale25 <rafale25.dev@gmail.com>
2024-11-03 20:37:19 +0100

    updated dependies + minor cleanup

--- slime.py
+++ slime.py
@@ -1,10 +1,7 @@
 #! /usr/bin/python3
 
-import sys
 import random
-import struct
-import time
-import pathlib
+from pathlib import Path
 
 import math
 from array import array
@@ -14,279 +11,270 @@ import imgui
 import glm
 
 import moderngl_window as mglw
-# from moderngl_window.conf import settings
 from moderngl_window.integrations.imgui import ModernglWindowRenderer
 
 from utils import *
-
 from _config import *
 
 class MyWindow(mglw.WindowConfig):
-	title = "Slime Simulation"
-	gl_version = (4, 3)
-	window_size = (Camera.ratio[0] * RATIO_MULT, Camera.ratio[1] * RATIO_MULT)
-	fullscreen = False
-	resizable = False
-	vsync = True
-	resource_dir = "./resources"
+    title = "Slime Simulation"
+    gl_version = (4, 3)
+    window_size = (Camera.ratio[0] * RATIO_MULT, Camera.ratio[1] * RATIO_MULT)
+    fullscreen = False
+    resizable = True
+    vsync = True
+    resource_dir = (Path(__file__) / "../resources").resolve()
 
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-		imgui.create_context()
-		self.imgui = ModernglWindowRenderer(self.wnd)
+        imgui.create_context()
+        self.imgui = ModernglWindowRenderer(self.wnd)
 
-		self.width, self.height = self.window_size
-		self.pause = True
+        self.width, self.height = self.window_size
+        self.pause = True
 
-		self.agent_config = AgentConfig()
-		self.profiles = []
+        self.agent_config = AgentConfig()
+        self.profiles = []
 
-		# texture
-		self.texture = self.ctx.texture(
-			size=TextureConfig.size,
-			data=array('f', [0.0, 0.0, 0.0, 1.0] * (TextureConfig.size[0] * TextureConfig.size[1])),
-			components=4,
-			dtype='f4')
+        # texture
+        self.texture = self.ctx.texture(
+            size=TextureConfig.size,
+            data=array('f', [0.0, 0.0, 0.0, 1.0] * (TextureConfig.size[0] * TextureConfig.size[1])),
+            components=4,
+            dtype='f4')
 
-		self.texture.repeat_x, self.texture.repeat_y = False, False
-		self.texture.filter = moderngl.NEAREST, moderngl.NEAREST
+        self.texture.repeat_x, self.texture.repeat_y = False, False
+        self.texture.filter = moderngl.NEAREST, moderngl.NEAREST
 
-		self.quad_2d = mglw.geometry.quad_2d(
-			size=(TextureConfig.size[0], TextureConfig.size[1]),
-			pos=(TextureConfig.size[0]/2, TextureConfig.size[1]/2),
-			normals=False)
-		self.shader_texture = self.load_program(
-			vertex_shader="./texture.vert",
-			fragment_shader="./texture.frag"
-		)
+        self.quad_2d = mglw.geometry.quad_2d(
+            size=(TextureConfig.size[0], TextureConfig.size[1]),
+            pos=(TextureConfig.size[0]/2, TextureConfig.size[1]/2),
+            normals=False)
+        self.shader_texture = self.load_program(
+            vertex_shader="./texture.vert",
+            fragment_shader="./texture.frag"
+        )
 
-		# agent : vec2 position, vec2 direction
-		self.buffer_agent = self.ctx.buffer(array('f', self.gen_initial_data(AgentConfig.N)))
+        # agent : vec2 position, vec2 direction
+        self.buffer_agent = self.ctx.buffer(array('f', self.gen_initial_data(AgentConfig.N)))
 
-		self.CS_agent = self.load_compute_shader(
-			'./agent.comp',
-			{
-				"texture_width": TextureConfig.size[0],
-				"texture_height": TextureConfig.size[1],
-				"l_size_x": 512,
-			}
-		)
-		self.CS_texture = self.load_compute_shader(
-			'./texture.comp',
-			{
-				"l_size_x": TextureConfig.local_size_x,
-				"l_size_y": TextureConfig.local_size_y,
-				"l_size_z": TextureConfig.local_size_z
-			}
-		)
+        self.CS_agent = self.load_compute_shader(
+            './agent.comp',
+            {
+                "texture_width": TextureConfig.size[0],
+                "texture_height": TextureConfig.size[1],
+                "l_size_x": 512,
+            }
+        )
+        self.CS_texture = self.load_compute_shader(
+            './texture.comp',
+            {
+                "l_size_x": TextureConfig.local_size_x,
+                "l_size_y": TextureConfig.local_size_y,
+                "l_size_z": TextureConfig.local_size_z
+            }
+        )
 
-		self.load_profiles("profiles.txt")
+        self.load_profiles("profiles.txt")
 
-	def __del__(self):
-		print("Cleaning up ressources.")
-		self.shader_texture.release()
-		self.buffer_agent.release()
-		self.CS_agent.release()
-		self.CS_texture.release()
+    from _profile import load_profiles, set_profile
 
-	from _profile import load_profiles, set_profile
+    def update_uniforms(self, time):
+        self.CS_agent['timer'] = int(time * 1000 % 2_147_483_647)
+        self.CS_agent['nb_agent'] = self.agent_config.N
+        self.CS_agent['speed'] = self.agent_config.speed
+        self.CS_agent['steerStrength'] = self.agent_config.steer
+        self.CS_agent['sensorAngleSpacing'] = self.agent_config.sensorAngleSpacing
+        self.CS_agent['sensorDistance'] = self.agent_config.sensorDistance
 
-	def update_uniforms(self, frametime):
-		self.CS_agent['timer'] = (time.time() * 100000) % 2_147_483_647 # dont exceed int_max
-		self.CS_agent['nb_agent'] = self.agent_config.N
-		self.CS_agent['speed'] = self.agent_config.speed
-		self.CS_agent['steerStrength'] = self.agent_config.steer
-		self.CS_agent['sensorAngleSpacing'] = self.agent_config.sensorAngleSpacing
-		self.CS_agent['sensorDistance'] = self.agent_config.sensorDistance
+        self.CS_texture['width'] = TextureConfig.size[0]
+        self.CS_texture['height'] = TextureConfig.size[1]
+        self.CS_texture['diffuse'] = TextureConfig.diffuse
+        self.CS_texture['evaporation'] = TextureConfig.evaporation
 
-		self.CS_texture['width'] = TextureConfig.size[0]
-		self.CS_texture['height'] = TextureConfig.size[1]
-		self.CS_texture['diffuse'] = TextureConfig.diffuse
-		self.CS_texture['evaporation'] = TextureConfig.evaporation
+        self.shader_texture['color_1'] = TextureConfig.color_1
+        self.shader_texture['color_2'] = TextureConfig.color_2
+        self.shader_texture['color_3'] = TextureConfig.color_3
 
-		self.shader_texture['color_1'] = TextureConfig.color_1
-		self.shader_texture['color_2'] = TextureConfig.color_2
-		self.shader_texture['color_3'] = TextureConfig.color_3
+    def update(self, time, frametime):
+        self.update_uniforms(time)
+        self.buffer_agent.bind_to_storage_buffer(0)
+        self.texture.bind_to_image(0, read=True, write=True)
 
-	def update(self, time, frametime):
-		self.update_uniforms(frametime)
-		self.buffer_agent.bind_to_storage_buffer(0)
-		self.texture.bind_to_image(0, read=True, write=True)
+        self.CS_agent.run(
+            group_x=AgentConfig.N // 512 + 1,
+            group_y=1,
+            group_z=1
+        )
 
-		self.CS_agent.run(
-			group_x=AgentConfig.N // 512 + 1,
-			group_y=1,
-			group_z=1
-		)
+        self.CS_texture.run(
+            group_x=TextureConfig.size[0] // TextureConfig.local_size_x + 1,
+            group_y=TextureConfig.size[1] // TextureConfig.local_size_y + 1,
+            group_z=1
+        )
 
-		self.CS_texture.run(
-			group_x=TextureConfig.size[0] // TextureConfig.local_size_x + 1,
-			group_y=TextureConfig.size[1] // TextureConfig.local_size_y + 1,
-			group_z=1
-		)
+    def render(self, time, frametime):
+        if not self.pause:
+            self.update(time, frametime)
 
-	def render(self, time, frametime):
-		if not self.pause:
-			self.update(time, frametime)
+        self.ctx.clear(0.5, 0.5, 0.5)
 
-		self.ctx.clear(0.5, 0.5, 0.5)
+        modelview = glm.ortho(0, TextureConfig.size[0], 0, TextureConfig.size[1], -1, 1)
+        self.shader_texture['modelview'].write(modelview)
 
-		modelview = glm.ortho(0, TextureConfig.size[0], 0, TextureConfig.size[1], -1, 1)
-		self.shader_texture['modelview'].write(modelview)
+        self.texture.use(location=0)
+        self.quad_2d.render(self.shader_texture)
 
-		self.texture.use(location=0)
-		self.quad_2d.render(self.shader_texture)
+        self.imgui_newFrame()
+        self.imgui_render()
 
-		self.imgui_newFrame()
-		self.imgui_render()
+    # -------------------------------------------------------------------------
+    def resize_buffer(self, new_count):
+        AGENT_SIZE_BYTES = 4 * 4
+        buffer = self.buffer_agent.read()[0:new_count * AGENT_SIZE_BYTES]
 
-	# -------------------------------------------------------------------------
-	def resize_buffer(self, new_count):
-		AGENT_SIZE_BYTES = 4 * 4
-		buffer = self.buffer_agent.read()[0:new_count * AGENT_SIZE_BYTES]
+        self.buffer_agent.orphan(new_count * AGENT_SIZE_BYTES)
 
-		self.buffer_agent.orphan(new_count * AGENT_SIZE_BYTES)
+        if new_count > AgentConfig.N:
+            new_bytes = array('f', self.gen_initial_data(new_count - AgentConfig.N))
+            buffer += new_bytes
 
-		if new_count > AgentConfig.N:
-			new_bytes = array('f', self.gen_initial_data(new_count - AgentConfig.N))
-			buffer += new_bytes
+        self.buffer_agent.write(buffer)
 
-		self.buffer_agent.write(buffer)
+        AgentConfig.N = new_count
 
-		AgentConfig.N = new_count
+    # data initialization
+    def gen_initial_data(self, count):
+        for _ in range(count):
+            # position
+            vec = random_uniform_vec2()
+            dist = random.uniform(0, 200)
+            yield TextureConfig.size[0]/2 + vec[0] * dist
+            yield TextureConfig.size[1]/2 + vec[1] * dist
 
-	# data initialization
-	def gen_initial_data(self, count):
-		for _ in range(count):
-			# position
-			vec = random_uniform_vec2()
-			dist = random.uniform(0, 200)
-			yield TextureConfig.size[0]/2 + vec[0] * dist
-			yield TextureConfig.size[1]/2 + vec[1] * dist
+            # direction
+            vec = random_uniform_vec2()
+            yield vec[0]
+            yield vec[1]
 
-			# direction
-			vec = random_uniform_vec2()
-			yield vec[0]
-			yield vec[1]
+    # -------------------------------------------------------------------------
+    # IMGUI
+    def imgui_newFrame(self):
+        imgui.new_frame()
+        imgui.begin("Properties", True)
 
-	# -------------------------------------------------------------------------
-	# IMGUI
-	def imgui_newFrame(self):
-		imgui.new_frame()
-		imgui.begin("Properties", True)
+        c, self.pause = imgui.checkbox("Paused", self.pause)
 
-		c, self.pause = imgui.checkbox("Paused", self.pause)
+        imgui.spacing();imgui.spacing();imgui.separator();imgui.spacing();imgui.spacing()
 
-		imgui.spacing();imgui.spacing();imgui.separator();imgui.spacing();imgui.spacing()
+        imgui.text("Agents Settings"); imgui.spacing()
+        imgui.begin_group()
+        c, new_N = imgui.slider_int(
+            label="N",
+            value=self.agent_config.N,
+            min_value=1,
+            max_value=1_000_000)
+        if c:
+            self.resize_buffer(new_N)
 
-		imgui.text("Agents Settings"); imgui.spacing()
-		imgui.begin_group()
-		c, new_N = imgui.slider_int(
-			label="N",
-			value=self.agent_config.N,
-			min_value=1,
-			max_value=1_000_000)
-		if c:
-			self.resize_buffer(new_N)
+        c, self.agent_config.speed = imgui.slider_float(
+            label="Speed",
+            value=self.agent_config.speed,
+            min_value=0.01,
+            max_value=10.0,
+            format="%.2f")
 
-		c, self.agent_config.speed = imgui.slider_float(
-			label="Speed",
-			value=self.agent_config.speed,
-			min_value=0.01,
-			max_value=10.0,
-			format="%.2f")
+        c, self.agent_config.steer = imgui.slider_float(
+            label="SteerStrength",
+            value=self.agent_config.steer,
+            min_value=0.01,
+            max_value=5.0,
+            format="%.3f")
 
-		c, self.agent_config.steer = imgui.slider_float(
-			label="SteerStrength",
-			value=self.agent_config.steer,
-			min_value=0.01,
-			max_value=5.0,
-			format="%.3f")
+        c, self.agent_config.sensorAngleSpacing = imgui.slider_float(
+            label="SensorAngleSpacing",
+            value=self.agent_config.sensorAngleSpacing,
+            min_value=0.1,
+            max_value=math.pi,
+            format="%.2f")
 
-		c, self.agent_config.sensorAngleSpacing = imgui.slider_float(
-			label="SensorAngleSpacing",
-			value=self.agent_config.sensorAngleSpacing,
-			min_value=0.1,
-			max_value=math.pi,
-			format="%.2f")
+        c, self.agent_config.sensorDistance = imgui.slider_int(
+            label="SensorDistance",
+            value=self.agent_config.sensorDistance,
+            min_value=0,
+            max_value=100)
 
-		c, self.agent_config.sensorDistance = imgui.slider_int(
-			label="SensorDistance",
-			value=self.agent_config.sensorDistance,
-			min_value=0,
-			max_value=100)
+        c, TextureConfig.color_1 = imgui.color_edit3("Color 1", *TextureConfig.color_1)
+        c, TextureConfig.color_2 = imgui.color_edit3("Color 2", *TextureConfig.color_2)
+        c, TextureConfig.color_3 = imgui.color_edit3("Color 3", *TextureConfig.color_3)
 
-		c, TextureConfig.color_1 = imgui.color_edit3("Color 1", *TextureConfig.color_1)
-		c, TextureConfig.color_2 = imgui.color_edit3("Color 2", *TextureConfig.color_2)
-		c, TextureConfig.color_3 = imgui.color_edit3("Color 3", *TextureConfig.color_3)
+        imgui.end_group()
 
-		imgui.end_group()
+        imgui.text("Texture Settings"); imgui.spacing()
+        imgui.begin_group()
+        c, TextureConfig.diffuse = imgui.slider_float(
+            label="Diffuse",
+            value=TextureConfig.diffuse,
+            min_value=0.0,
+            max_value=1.0,
+            format="%.2f")
 
-		imgui.text("Texture Settings"); imgui.spacing()
-		imgui.begin_group()
-		c, TextureConfig.diffuse = imgui.slider_float(
-			label="Diffuse",
-			value=TextureConfig.diffuse,
-			min_value=0.0,
-			max_value=1.0,
-			format="%.2f")
+        c, TextureConfig.evaporation = imgui.slider_float(
+            label="Decay",
+            value=TextureConfig.evaporation,
+            min_value=0.0,
+            max_value=0.1,
+            format="%.4f")
+        imgui.end_group()
 
-		c, TextureConfig.evaporation = imgui.slider_float(
-			label="Decay",
-			value=TextureConfig.evaporation,
-			min_value=0.0,
-			max_value=0.1,
-			format="%.4f")
-		imgui.end_group()
+        imgui.begin_group()
+        for profile in self.profiles:
+            if (imgui.button(profile)):
+                self.set_profile(self.profiles[profile])
 
-		imgui.begin_group()
-		for profile in self.profiles:
-			if (imgui.button(profile)):
-				self.set_profile(self.profiles[profile])
+        imgui.end_group()
 
-		imgui.end_group()
+        imgui.end()
 
-		imgui.end()
+    def imgui_render(self):
+        imgui.render()
+        self.imgui.render(imgui.get_draw_data())
 
-	def imgui_render(self):
-		imgui.render()
-		self.imgui.render(imgui.get_draw_data())
+    # -------------------------------------------------------------------------
+    # EVENTS
+    def resize(self, width: int, height: int):
+        self.imgui.resize(width, height)
 
-	# -------------------------------------------------------------------------
-	# EVENTS
-	def resize(self, width: int, height: int):
-		self.imgui.resize(width, height)
+    def key_event(self, key, action, modifiers):
+        self.imgui.key_event(key, action, modifiers)
 
-	def key_event(self, key, action, modifiers):
-		self.imgui.key_event(key, action, modifiers)
+    def mouse_position_event(self, x, y, dx, dy):
+        self.imgui.mouse_position_event(x, y, dx, dy)
 
-	def mouse_position_event(self, x, y, dx, dy):
-		self.imgui.mouse_position_event(x, y, dx, dy)
+    def mouse_drag_event(self, x, y, dx, dy):
+        self.imgui.mouse_drag_event(x, y, dx, dy)
+        Camera.center[0] += -dx
+        Camera.center[1] += dy
 
-	def mouse_drag_event(self, x, y, dx, dy):
-		self.imgui.mouse_drag_event(x, y, dx, dy)
-		Camera.center[0] += -dx
-		Camera.center[1] += dy
+    def mouse_scroll_event(self, x_offset, y_offset):
+        self.imgui.mouse_scroll_event(x_offset, y_offset)
+        Camera.zoom += y_offset * 10
+        if Camera.zoom < 1.0:
+            Camera.zoom = 1.0
 
-	def mouse_scroll_event(self, x_offset, y_offset):
-		self.imgui.mouse_scroll_event(x_offset, y_offset)
-		Camera.zoom += y_offset * 10
-		if Camera.zoom < 1.0:
-			Camera.zoom = 1.0
+    def mouse_press_event(self, x, y, button):
+        self.imgui.mouse_press_event(x, y, button)
 
-	def mouse_press_event(self, x, y, button):
-		self.imgui.mouse_press_event(x, y, button)
+    def mouse_release_event(self, x: int, y: int, button: int):
+        self.imgui.mouse_release_event(x, y, button)
 
-	def mouse_release_event(self, x: int, y: int, button: int):
-		self.imgui.mouse_release_event(x, y, button)
-
-	def unicode_char_entered(self, char):
-		self.imgui.unicode_char_entered(char)
+    def unicode_char_entered(self, char):
+        self.imgui.unicode_char_entered(char)
 
 
 def main():
-	MyWindow.run()
+    MyWindow.run()
 
 if __name__ == "__main__":
-	main()
+    main()
--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ from random import uniform
 from math import pi, cos, sin
 
 def random_uniform_vec2():
-	angle = uniform(-pi, pi);
-	return cos(angle), sin(angle);
+	angle = uniform(-pi, pi)
+	return cos(angle), sin(angle)
